dafde6ee521030af2474d1cb53c443bbd9f23470
d1m0ne <71107177+d1m0ne@users.noreply.github.com>
2020-09-11 14:38:06 -0400

    Add files via upload

--- Python_assignment_3.5.py
+++ Python_assignment_3.5.py
@@ -5,7 +5,10 @@ print("guess the number!")
 guess = input()
 
 while int(Numberguess) != int(guess):
-    print("guess again!")
+    if int(Numberguess) > int(guess):
+        print("Your number is too low, guess again!")
+    elif int(Numberguess) < int(guess):
+        print("Your number is too high, guess again!")
     guess = input()
 
 print("You guessed the number!")
